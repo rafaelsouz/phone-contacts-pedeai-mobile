73c67d9c2ed583e7b8cf6c90ffb1f5cd8ffad560
Rafael Souza <raffaelsouza908@gmail.com>
2020-07-01 20:41:59 -0300

    Adicionando os componentes botão e o input

--- src/pages/SigIn/index.tsx
+++ src/pages/SigIn/index.tsx
@@ -1,14 +1,30 @@
 import React from 'react';
 import { Image } from 'react-native';
 
-import { Container } from './styles';
-
 import logoImg from '../../assets/logo.png';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import { Container, Title } from './styles';
 
 const SigIn: React.FC = () => {
   return (
     <Container>
-      <Image source={logoImg} />
+      <Image source={logoImg} style={{ height: 150, resizeMode: 'contain' }} />
+
+      <Title>Faça seu login</Title>
+
+      <Input name="email" icon="mail" placeholder="E-mail" />
+      <Input name="password" icon="lock" placeholder="Senha" />
+
+      <Button
+        onPress={() => {
+          console.log('Button');
+        }}
+      >
+        Entrar
+      </Button>
     </Container>
   );
 };
