0de068c7b7e11349b6bb33e11145efb1b63cf6c5
Rafael Souza <raffaelsouza908@gmail.com>
2020-07-01 22:20:45 -0300

    Alterando nome do arquivo de SignIn

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 
-import SignIn from '../pages/SigIn';
+import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
 
 const Auth = createStackNavigator();
