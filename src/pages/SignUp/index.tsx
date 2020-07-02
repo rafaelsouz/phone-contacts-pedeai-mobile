768054c10f869523b88a36f797b11ba9581850e6
Rafael Souza <raffaelsouza908@gmail.com>
2020-07-01 22:20:20 -0300

    Criando e estilizando tela de cadastro de usuário.

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,12 +1,70 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import {
+  Image,
+  View,
+  ScrollView,
+  KeyboardAvoidingView,
+  Platform,
+} from 'react-native';
 
-// import { Container } from './styles';
+import { useNavigation } from '@react-navigation/native';
+
+import Icon from 'react-native-vector-icons/Feather';
+
+import logoImg from '../../assets/logo.png';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import { Container, Title, BackToSignIn, BackToSignInText } from './styles';
 
 const SignUp: React.FC = () => {
+  const navigation = useNavigation();
+
   return (
     <>
-      <Text>SignUp</Text>
+      <KeyboardAvoidingView
+        enabled
+        style={{ flex: 1 }}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+      >
+        <ScrollView
+          keyboardShouldPersistTaps="handled"
+          contentContainerStyle={{ flexGrow: 1, justifyContent: 'center' }}
+        >
+          <Container>
+            <Image
+              source={logoImg}
+              style={{ height: 150, resizeMode: 'contain' }}
+            />
+
+            <View>
+              <Title>Crie sua conta</Title>
+            </View>
+
+            <Input name="name" icon="user" placeholder="Nome" />
+            <Input name="email" icon="mail" placeholder="E-mail" />
+            <Input name="password" icon="lock" placeholder="Senha" />
+
+            <Button
+              onPress={() => {
+                console.log('Button');
+              }}
+            >
+              Entrar
+            </Button>
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
+
+      <BackToSignIn
+        onPress={() => {
+          navigation.goBack();
+        }}
+      >
+        <Icon name="arrow-left" size={20} color="#fff" />
+        <BackToSignInText>Voltar para login</BackToSignInText>
+      </BackToSignIn>
     </>
   );
 };
--- src/pages/SignUp/styles.ts
+++ src/pages/SignUp/styles.ts
@@ -1,3 +1,39 @@
 import styled from 'styled-components/native';
+import { Platform } from 'react-native';
+import { getBottomSpace } from 'react-native-iphone-x-helper';
 
-export const Container = styled.View``;
+export const Container = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+  padding: 0 30px ${Platform.OS === 'android' ? 80 : 40}px;
+`;
+
+export const Title = styled.Text`
+  font-size: 24px;
+  color: #fff;
+  font-family: sans-serif;
+  margin: 54px 0 24px;
+`;
+
+export const BackToSignIn = styled.TouchableOpacity`
+  position: absolute;
+  left: 0;
+  right: 0;
+  bottom: 0;
+
+  background: #312e38;
+  border-top-width: 2px;
+  border-color: #232129;
+  padding: 16px 0 ${14 + getBottomSpace()}px;
+
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+`;
+export const BackToSignInText = styled.Text`
+  color: #fff;
+  font-size: 18px;
+  font-family: sans-serif;
+  margin-left: 16px;
+`;
