a24a66114283e5eb01a61d8774146ede8c8430b3
Rafael Souza <raffaelsouza908@gmail.com>
2020-07-03 23:28:42 -0300

    Criando pagina de perfil do contato

---
+++ src/pages/ContactProfile/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+// import { Container } from './styles';
+
+const ContactProfile: React.FC = () => {
+  return (
+    <View>
+      <Text>ContactProfile</Text>
+    </View>
+  );
+};
+
+export default ContactProfile;
