378bb2f583287f58f2ce9ad35430d91d9912740d
Rafael Souza <raffaelsouza908@gmail.com>
2020-07-03 23:29:20 -0300

    Criando uma interface para usuário

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -9,9 +9,15 @@ import AsyncStorage from '@react-native-community/async-storage';
 
 import api from '../services/api';
 
+interface User {
+  id: string;
+  name: string;
+  email: string;
+}
+
 interface AuthState {
   token: string;
-  user: Record<string, unknown>;
+  user: User;
 }
 
 interface SignInCredentials {
@@ -20,7 +26,7 @@ interface SignInCredentials {
 }
 
 interface AuthContextDATA {
-  user: Record<string, unknown>;
+  user: User;
   loading: boolean;
   signIn(credentials: SignInCredentials): Promise<void>;
   signOut(): void;
@@ -40,7 +46,9 @@ const AuthProvider: React.FC = ({ children }) => {
       ]);
 
       if (token[1] && user[1]) {
-        ({ token: token[1], user: JSON.parse(user[1]) });
+        api.defaults.headers.authorization = `Bearer ${token[1]}`;
+
+        setData({ token: token[1], user: JSON.parse(user[1]) });
       }
 
       setLoading(false);
@@ -61,6 +69,8 @@ const AuthProvider: React.FC = ({ children }) => {
       ['@GoBarber:token', token],
       ['@GoBarber:user', JSON.stringify(user)],
     ]);
+
+    api.defaults.headers.authorization = `Bearer ${token}`;
 
     setData({ token, user });
   }, []);
