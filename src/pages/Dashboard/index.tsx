abebe6fe32ddcdb98f17345cb6ccffb948800d0f
Rafael Souza <raffaelsouza908@gmail.com>
2020-07-03 23:29:32 -0300

    Estilizando pagina de Dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,16 +1,89 @@
-import React from 'react';
-import { View, Button } from 'react-native';
+import React, { useEffect, useState, useCallback } from 'react';
+import { useNavigation } from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/Feather';
+
+import api from '../../services/api';
 
 import { useAuth } from '../../hooks/auth';
-// import { Container } from './styles';
+
+import {
+  Container,
+  Header,
+  HeaderTitle,
+  UserName,
+  ContactsList,
+  ContactContainer,
+  ContactName,
+  ContactInfo,
+  ContactMeta,
+  ContactMetaText,
+  ContactsListTitle,
+} from './styles';
+
+export interface Contact {
+  id: string;
+  name: string;
+  phone: string;
+  email: string;
+}
 
 const Dashboard: React.FC = () => {
-  const { signOut } = useAuth();
+  const { signOut, user } = useAuth();
+
+  const [contacts, setContacts] = useState<Contact[]>([]);
+
+  const { navigate } = useNavigation();
+
+  useEffect(() => {
+    api.get('contacts').then(response => {
+      setContacts(response.data);
+    });
+  }, []);
+
+  const navigateToContactProfile = useCallback(
+    (contactId: string) => {
+      navigate('ContactProfile', { contactId });
+    },
+    [navigate],
+  );
 
   return (
-    <View>
-      <Button title="Sair" onPress={signOut} />
-    </View>
+    <Container>
+      <Header>
+        <HeaderTitle>
+          Bem Vindo,
+          {'\n'}
+          <UserName>{user.name}</UserName>
+        </HeaderTitle>
+      </Header>
+
+      <ContactsList
+        data={contacts}
+        keyExtractor={contact => contact.id}
+        ListHeaderComponent={<ContactsListTitle>Contatos</ContactsListTitle>}
+        renderItem={({ item: contact }) => (
+          <ContactContainer
+            onPress={() => {
+              navigateToContactProfile(contact.id);
+            }}
+          >
+            <ContactInfo>
+              <ContactName>{contact.name}</ContactName>
+
+              <ContactMeta>
+                <Icon name="user" size={14} color="#ff3030" />
+                <ContactMetaText>{contact.email}</ContactMetaText>
+              </ContactMeta>
+
+              <ContactMeta>
+                <Icon name="phone" size={14} color="#ff3030" />
+                <ContactMetaText>{contact.phone}</ContactMetaText>
+              </ContactMeta>
+            </ContactInfo>
+          </ContactContainer>
+        )}
+      />
+    </Container>
   );
 };
 
---
+++ src/pages/Dashboard/styles.ts
@@ -0,0 +1,70 @@
+import styled from 'styled-components/native';
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
+import { FlatList } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
+import { Contact } from './index';
+
+export const Container = styled.View`
+  flex: 1;
+`;
+
+export const Header = styled.View`
+  padding: 24px;
+  padding-top: ${getStatusBarHeight()}px;
+  background: #28262e;
+
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const HeaderTitle = styled.Text`
+  color: #f4ede8;
+  font-size: 20px;
+  font-family: sans-serif;
+  line-height: 28px;
+`;
+
+export const UserName = styled.Text`
+  color: #ff3030;
+  font-weight: bold;
+`;
+
+export const ContactsListTitle = styled.Text`
+  font-size: 24px;
+  margin-bottom: 24px;
+  color: #f4ede8;
+  font-family: sans-serif;
+`;
+
+export const ContactsList = styled(FlatList as new () => FlatList<Contact>)`
+  padding: 32px 24px 16px;
+`;
+
+export const ContactContainer = styled(RectButton)`
+  background: #3e3b47;
+  border-radius: 10px;
+  padding: 20px;
+  margin-bottom: 16px;
+  flex-direction: row;
+  align-items: center;
+`;
+export const ContactInfo = styled.View`
+  flex: 1;
+  margin-left: 20px;
+`;
+export const ContactName = styled.Text`
+  font-family: sans-serif;
+  font-size: 18px;
+  color: #f4ede8;
+`;
+export const ContactMeta = styled.View`
+  flex-direction: row;
+  align-items: center;
+  margin-top: 8px;
+`;
+export const ContactMetaText = styled.Text`
+  margin-left: 8px;
+  color: #999591;
+  font-family: sans-serif;
+`;
