ac6ad3f349ecea57fc1178df7735b1cec8d687ce
Rafael Souza <raffaelsouza908@gmail.com>
2020-07-05 13:13:16 -0300

    Perfil do contato

--- src/pages/ContactProfile/index.tsx
+++ src/pages/ContactProfile/index.tsx
@@ -1,13 +1,110 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useCallback, useEffect, useState } from 'react';
+import { ScrollView, KeyboardAvoidingView, Platform } from 'react-native';
 
-// import { Container } from './styles';
+import { useNavigation, useRoute } from '@react-navigation/native';
+
+import Icon from 'react-native-vector-icons/Feather';
+
+import api from '../../services/api';
+
+import Button from '../../components/Button';
+
+import {
+  Container,
+  ContatInfo,
+  ContactListInfo,
+  ContactInfoText,
+  Header,
+  BackButtom,
+  HeaderTitle,
+  ContactContainer,
+} from './styles';
+
+interface RouteParams {
+  contactId: string;
+}
+
+export interface Contact {
+  id: string;
+  name: string;
+  phone: string;
+  email: string;
+}
 
 const ContactProfile: React.FC = () => {
+  const { navigate, goBack } = useNavigation();
+  const route = useRoute();
+  const { contactId } = route.params as RouteParams;
+
+  const [contact, setContact] = useState<Contact>({} as Contact);
+
+  const navigateToEditContact = useCallback(
+    (idContact: string) => {
+      navigate('EditContact', { idContact });
+    },
+    [navigate],
+  );
+
+  const navigateBack = useCallback(() => {
+    goBack();
+  }, [goBack]);
+
+  useEffect(() => {
+    api.get(`/contacts/${contactId}`).then(response => {
+      setContact(response.data);
+    });
+  }, [contactId]);
+
   return (
-    <View>
-      <Text>ContactProfile</Text>
-    </View>
+    <>
+      <KeyboardAvoidingView
+        enabled
+        style={{ flex: 1 }}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+      >
+        <ScrollView
+          keyboardShouldPersistTaps="handled"
+          contentContainerStyle={{ flexGrow: 1, justifyContent: 'center' }}
+        >
+          <Container>
+            <Header>
+              <BackButtom
+                onPress={() => {
+                  navigateBack();
+                }}
+              >
+                <Icon name="chevron-left" size={26} color="#999591" />
+              </BackButtom>
+              <HeaderTitle>{contact.name}</HeaderTitle>
+            </Header>
+
+            <ContactContainer>
+              <ContactListInfo>
+                <ContatInfo>
+                  <Icon name="phone" size={24} color="#ff3030" />
+                  <ContactInfoText>{contact.phone}</ContactInfoText>
+                </ContatInfo>
+
+                <ContatInfo>
+                  <Icon name="mail" size={24} color="#ff3030" />
+                  <ContactInfoText>{contact.email}</ContactInfoText>
+                </ContatInfo>
+              </ContactListInfo>
+
+              <Button
+                onPress={() => {
+                  navigateToEditContact(contact.id);
+                }}
+              >
+                Editar
+              </Button>
+
+              <Button onPress={() => {}}>Ligar</Button>
+            </ContactContainer>
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
+    </>
   );
 };
 
--- src/pages/ContactProfile/styles.ts
+++ src/pages/ContactProfile/styles.ts
@@ -1,3 +1,56 @@
 import styled from 'styled-components/native';
+import { Platform } from 'react-native';
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
 
-export const Container = styled.View``;
+export const Container = styled.View`
+  flex: 1;
+`;
+
+export const Header = styled.View`
+  padding: 24px;
+  padding-top: ${getStatusBarHeight()}px;
+  background: #28262e;
+
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const BackButtom = styled.TouchableOpacity``;
+
+export const HeaderTitle = styled.Text`
+  color: #f5ede8;
+  font-family: sans-serif;
+  font-size: 24px;
+  margin-left: 16px;
+`;
+
+export const ContactListInfo = styled.View`
+  margin-bottom: 24px;
+`;
+
+export const ContactContainer = styled.View`
+  padding: 0 30px;
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const ContatInfo = styled.View`
+  min-width: 100%;
+  height: 60px;
+  padding: 0 16px;
+  border-radius: 10px;
+  margin-bottom: 8px;
+  border-bottom-width: 2px;
+  border-color: #232129;
+
+  flex-direction: row;
+  align-items: center;
+  justify-content: flex-start;
+`;
+
+export const ContactInfoText = styled.Text`
+  font-size: 20px;
+  color: #fff;
+  margin-left: 24px;
+`;
